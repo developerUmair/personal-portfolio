737ad7fa1ce61947717723ce7aedf51433593338
developerUmair <umairmak143@gmail.com>
2024-10-21 13:03:36 +0500

    initialize tape section

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,3 +1,27 @@
+import StarIcon from "@/assets/icons/star.svg";
+
+const words = [
+  "Performant",
+  "Accessible",
+  "Secure",
+  "Interactive",
+  "Scalable",
+  "User Friendly",
+  "Maintainable",
+  "Search Optimized",
+  "Usable",
+  "Reliable",
+];
+
 export const TapeSection = () => {
-  return <div>Tape Section</div>;
+  return (
+    <div className="flex items-center overflow-scroll">
+      {words.map((word) => (
+        <div key={word} className="flex items-center gap-x-3">
+          <span>{word}</span>
+          <StarIcon />
+        </div>
+      ))}
+    </div>
+  );
 };
