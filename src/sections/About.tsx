fc9a60e0f4e72f85722ddc5730c74807d79fd1a2
developerUmair <umairmak143@gmail.com>
2024-10-28 16:09:23 +0500

    initialize about use section

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,3 +1,72 @@
+import { Card } from "@/components/Card";
+import { SectionHeader } from "@/components/SectionHeader";
+import StarIcon from "@/assets/icons/star.svg";
+import Image from "next/image";
+import BookImage from "@/assets/images/book-cover.png";
+import JavascriptIcon from "@/assets/icons/square-js.svg";
+import HTMLIcon from "@/assets/icons/html5.svg";
+import CssIcon from "@/assets/icons/css3.svg";
+import ReactIcon from "@/assets/icons/react.svg";
+import ChromeIcon from "@/assets/icons/chrome.svg";
+import GithubIcon from "@/assets/icons/github.svg";
+import { TechIcon } from "@/components/TechIcon";
+import mapImage from "@/assets/images/map.png";
+import smileMemoji from "@/assets/images/memoji-smile.png";
+
+const toolboxItems = [
+  { title: "JavaScript", iconType: JavascriptIcon },
+  { title: "HTML5", iconType: HTMLIcon },
+  { title: "CSS3", iconType: CssIcon },
+  { title: "React", iconType: ReactIcon },
+  { title: "Chrome", iconType: ChromeIcon },
+  { title: "GitHub", iconType: GithubIcon },
+];
+
 export const AboutSection = () => {
-  return <div>About Section</div>;
+  return (
+    <div>
+      <SectionHeader
+        eyebrow="About Me"
+        title="A Glimpse Into My World"
+        description="Learn more about who I am, What I do and what inspires me."
+      />
+      <div>
+        <Card>
+          <div>
+            <StarIcon />
+            <h3>My Reads</h3>
+            <p>Explore the books shaping my perspectives.</p>
+          </div>
+          <Image src={BookImage} alt="book image" />
+        </Card>
+        <Card>
+          <div>
+            <StarIcon />
+            <h3>My Tools</h3>
+            <p>
+              Explore the technologies and tools I use to craft exceptional
+              digital experiences.
+            </p>
+          </div>
+          {toolboxItems.map((item) => (
+            <div key={item.title}>
+              <TechIcon component={item.iconType} />
+              <span>{item.title}</span>
+            </div>
+          ))}
+        </Card>
+        <Card>
+          <div>
+            <StarIcon />
+            <h3>Beyond the Code.</h3>
+            <p>Explore my interests and hobbies beyond the digital realm.</p>
+          </div>
+        </Card>
+        <Card>
+          <Image src={mapImage} alt="map" />
+          <Image src={smileMemoji} alt="smiling memoji" />
+        </Card>
+      </div>
+    </div>
+  );
 };
