ee2f05e58e68f4435c3dae89f5c5374c5f9e23e4
developerUmair <umairmak143@gmail.com>
2024-10-28 16:52:01 +0500

    styling the my reads section for mobile

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -22,50 +22,77 @@ const toolboxItems = [
   { title: "GitHub", iconType: GithubIcon },
 ];
 
+const hobbies = [
+  { title: "Painting", emoji: "🎨" },
+  { title: "Photograph", emoji: "📷" },
+  { title: "Gaming", emoji: "🎮" },
+  { title: "Hiking", emoji: "🥾" },
+  { title: "Music", emoji: "🎵" },
+  { title: "Fitness", emoji: "🏋️‍♂️" },
+  { title: "Reading", emoji: "📚" },
+];
+
 export const AboutSection = () => {
   return (
-    <div>
-      <SectionHeader
-        eyebrow="About Me"
-        title="A Glimpse Into My World"
-        description="Learn more about who I am, What I do and what inspires me."
-      />
-      <div>
-        <Card>
-          <div>
-            <StarIcon />
-            <h3>My Reads</h3>
-            <p>Explore the books shaping my perspectives.</p>
-          </div>
-          <Image src={BookImage} alt="book image" />
-        </Card>
-        <Card>
-          <div>
-            <StarIcon />
-            <h3>My Tools</h3>
-            <p>
-              Explore the technologies and tools I use to craft exceptional
-              digital experiences.
-            </p>
-          </div>
-          {toolboxItems.map((item) => (
-            <div key={item.title}>
-              <TechIcon component={item.iconType} />
-              <span>{item.title}</span>
+    <div className="py-20">
+      <div className="container">
+        <SectionHeader
+          eyebrow="About Me"
+          title="A Glimpse Into My World"
+          description="Learn more about who I am, What I do and what inspires me."
+        />
+        <div className="mt-20">
+          <Card className="h-[320px]">
+            <div className="flex flex-col">
+              <div className="inline-flex items-center gap-2">
+                {" "}
+                <StarIcon className="size-9 text-emerald-300" />
+                <h3 className="font-serif text-3xl">My Reads</h3>
+              </div>
+              <p className="text-sm text-white/60 mt-2">
+                Explore the books shaping my perspectives.
+              </p>
             </div>
-          ))}
-        </Card>
-        <Card>
-          <div>
-            <StarIcon />
-            <h3>Beyond the Code.</h3>
-            <p>Explore my interests and hobbies beyond the digital realm.</p>
-          </div>
-        </Card>
-        <Card>
-          <Image src={mapImage} alt="map" />
-          <Image src={smileMemoji} alt="smiling memoji" />
-        </Card>
+            <div className="w-40 mx-auto mt-8">
+              <Image src={BookImage} alt="book image" />
+            </div>
+          </Card>
+          <Card>
+            <div>
+              <StarIcon />
+              <h3>My Tools</h3>
+              <p>
+                Explore the technologies and tools I use to craft exceptional
+                digital experiences.
+              </p>
+            </div>
+            {toolboxItems.map((item) => (
+              <div key={item.title}>
+                <TechIcon component={item.iconType} />
+                <span>{item.title}</span>
+              </div>
+            ))}
+          </Card>
+          <Card>
+            <div>
+              <StarIcon />
+              <h3>Beyond the Code.</h3>
+              <p>Explore my interests and hobbies beyond the digital realm.</p>
+            </div>
+            <div>
+              {hobbies.map((hobby) => (
+                <div key={hobby.title}>
+                  <span>{hobby.title}</span>
+                  <span>{hobby.emoji}</span>
+                </div>
+              ))}
+            </div>
+          </Card>
+          <Card>
+            <Image src={mapImage} alt="map" />
+            <Image src={smileMemoji} alt="smiling memoji" />
+          </Card>
+        </div>
       </div>
     </div>
   );
